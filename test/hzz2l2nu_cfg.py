e215bc85d2661645ff6f6bbdf1239a0e3c196e44
Xin Shi <shi210@purdue.edu>
2014-04-16 09:18:59 -0400

    add suffix

--- test/hzz2l2nu_cfg.py
+++ test/hzz2l2nu_cfg.py
@@ -3,6 +3,7 @@ import FWCore.ParameterSet.Config as cms
 runProcess = cms.PSet(
     input = cms.vstring("../data/ZZJetsTo2L2Nu.root"),
     outdir = cms.string("tmp"),
+    suffix = cms.string("_test"),
     dirName = cms.string("dataAnalyzer"),
     isMC = cms.bool(True),
     debug = cms.bool(True)	
