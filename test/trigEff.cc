95dfde89c63b1fb4c46331895970e6491aa3a553
Xin Shi <xshi@orca4>
2014-07-01 11:37:55 -0400

    draw efficiency

--- test/trigEff.cc
+++ test/trigEff.cc
@@ -14,15 +14,66 @@
 #include <TStyle.h> 
 #include <TProfile.h> 
 #include <TPaveText.h> 
-#include "tdrStyle.h"
+// #include "tdrStyle.h"
+#include <TApplication.h> 
+#include <TEfficiency.h> 
+
 
 using namespace std;
 
+void set_root_style(int stat=1110, int grid=0){
+  gROOT->Reset();
 
-void trigEff(TString inputFile, TString outFile) {
-  setTDRStyle(); 
+  gStyle->SetTitleFillColor(0) ; 
+  gStyle->SetTitleBorderSize(0); 
+    
+  gStyle->SetCanvasBorderMode(0);
+  gStyle->SetCanvasColor(0);
+  gStyle->SetCanvasDefX(0); 
+  gStyle->SetCanvasDefY(0); 
+  gStyle->SetFrameBorderMode(0); 
+  gStyle->SetFrameBorderSize(1); 
+  gStyle->SetFrameFillColor(0); 
+  gStyle->SetFrameFillStyle(0); 
+  gStyle->SetFrameLineColor(1); 
+  gStyle->SetFrameLineStyle(1); 
+  gStyle->SetFrameLineWidth(1); 
+
+  // gStyle->SetPadTopMargin(PadTopMargin);  
+  gStyle->SetPadLeftMargin(0.10);  
+  gStyle->SetPadRightMargin(0.05);  
+
+  gStyle->SetLabelSize(0.03, "XYZ");  
+  gStyle->SetTitleSize(0.04, "XYZ");  
+  gStyle->SetTitleOffset(1.2, "Y");  
+
+  gStyle->SetPadBorderMode(0);  
+  gStyle->SetPadColor(0);  
+  gStyle->SetPadTickX(1); 
+  gStyle->SetPadTickY(1); 
+  gStyle->SetPadGridX(grid); 
+  gStyle->SetPadGridY(grid); 
+
+  gStyle->SetOptStat(stat); 
+  gStyle->SetStatColor(0); 
+  gStyle->SetStatBorderSize(1); 
+}
+
+//void trigEff(TString inputFile, TString outFile) {
+void trigEff(TString inputFile) {
+  // setTDRStyle(); 
+  set_root_style(); 
 
   cout << "inputFile: " << inputFile << endl; 
+
+  TFile *f = new TFile(inputFile);
+  
+  TEfficiency *eff = (TEfficiency*)f->Get("eff");
+
+  TCanvas *c = new TCanvas("c", "c", 800, 800);
+
+  eff->Draw();
+  c->Update();
   
   //open the file and get events tree
   // DataEventSummaryHandler evSummaryHandler;
@@ -31,8 +82,7 @@ void trigEff(TString inputFile, TString outFile) {
 }
 
 
-#ifndef __CINT__ 
-#include <algorithm>
+// #include <algorithm>
 
 char* get_option(char ** begin, char ** end, const std::string & option){
   char ** itr = std::find(begin, end, option);
@@ -46,7 +96,7 @@ bool option_exists(char** begin, char** end, const std::string& option){
 
 
 void print_usage(){
-  cerr << "Usage: trigEff HLT_Path_name inputFile [test.pdf]\n" 
+  cerr << "Usage: trigEff inputFile \n" 
        << endl; 
 }
 
@@ -58,15 +108,16 @@ int main(int argc, char** argv) {
   }
   
   TString inputFile = argv[1]; 
-  TString outFile = "test.pdf"; 
-  if (argc >= 3) outFile = argv[2]; 
-  trigEff(inputFile, outFile);
-
-  gSystem->Exit(0);
-  
-  return 0 ;
+  // TString outFile = "test.pdf"; 
+  // if (argc >= 3) outFile = argv[2]; 
+  TApplication theApp("App", 0, 0);
+  theApp.SetReturnFromRun(true);
+  // trigEff(inputFile, outFile);
+  trigEff(inputFile);
+  theApp.Run();
+ 
+  // return 0 ;
 }
 
-#endif
 
 
