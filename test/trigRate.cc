d56441ef608d61fc6c8a62b1a3dbdb8f325ec859
Xin Shi <shi210@purdue.edu>
2014-07-20 10:45:46 -0400

    start

---
+++ test/trigRate.cc
@@ -0,0 +1,15 @@
+// 
+// Author: Xin Shi <Xin.Shi@cern.ch> 
+// 2014.07.20
+// 
+
+#include <iostream>
+#include <algorithm>
+
+int main(int argc, char** argv) {
+  std::cout << "Calculatine trigger rate ... " << std::endl; 
+  return 0 ;
+}
+
+
+
