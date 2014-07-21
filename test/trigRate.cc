bfdecfbde880e4b6ddd11bee644d2b3ab23e785e
Xin Shi <xshi@orca4>
2014-07-21 14:29:34 -0400

    print out the rates

--- test/trigRate.cc
+++ test/trigRate.cc
@@ -18,15 +18,29 @@ int main(int argc, char** argv) {
   double rate;
   double prescale;
 
+  double factor_pfmet, factor_vbf; 
   std::string line;
   int nlines = 0;
 
   while (getline(in, line)) {
+    if (!in.good()) break;
     std::istringstream iss(line);
     if ( line.find("#") == 0 ) continue; 
-    if (!(iss >> path >> rate >> prescale )) break; 
-    if (!in.good()) break;
-    std::cout << path << ": " << rate*prescale << std::endl; 
+    if (!(iss >> path >> rate >> prescale >> factor_pfmet >> factor_vbf )) break; 
+
+    // std::cout << path << ": " << rate*prescale << " Hz, " 
+    // 	      << factor_pfmet << " (PFMET) " 
+    // 	      << factor_vbf << " (VBF)" << std::endl; 
+
+    printf("%s : %.2f Hz (PFMET) %.2f Hz (VBF)\n",
+	   path.c_str(),
+	   rate*prescale/factor_pfmet,
+	   rate*prescale/factor_vbf); 
+
+    // std::cout << path << ": " // << rate*prescale 
+    // 	      << rate*prescale/factor_pfmet << " (PFMET) "
+    // 	      << rate*prescale/factor_vbf << " (VBF)" << std::endl; 
+    
     nlines ++; 
   }
 
