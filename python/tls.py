a40f6e1431e0a6fbb078ada86c58d7da3637c2aa
Xin Shi <xshi@xshi.org>
2014-11-12 00:08:29 -0500

    get stdout files

--- python/tls.py
+++ python/tls.py
@@ -10,6 +10,7 @@ __copyright__ = "Copyright (c) Xin Shi"
 
 import os
 import sys
+import subprocess
 import tls
 
 
@@ -31,8 +32,64 @@ def main():
     function = getattr(tls, args[0])
     function(args[1:])
 
+    
 def hlt_get_passed_evts(args):
-    print args 
+    resdir = '%s/res' %args[0]
+    files = get_files_in_dir(resdir, pattern='.stdout') 
+    print files
+    print len(files)
+    
+    
+#----------------------------------------------------------------
+#   Supporting function 
+#----------------------------------------------------------------
+
+def get_files_in_dir(path, pattern=None):
+    files = []
+    cmd = 'ls %s' %path
+    output = proc_cmd(cmd)
+    if 'No such file or directory' in output:
+        sys.stdout.write(output)
+        sys.stdout.write('Please check cmd: \n')
+        sys.stdout.write(cmd)
+        sys.stdout.write('\n')
+        sys.exit()
+
+    content = output.strip()
+    lines = content.split('\n')
+    for l in lines:
+        if pattern is None or pattern in l:
+            files.append(l)
+    return files
+
+
+def proc_cmd(cmd, test=False, verbose=1, procdir=None, shell=False):
+    if test:
+        sys.stdout.write(cmd+'\n')
+        return 
+
+    # No need to add quote in the cmd! 
+    cwd = os.getcwd()
+    if procdir is not None:
+        os.chdir(procdir)
+    
+    args = cmd.split()
+
+    if isinstance(cmd, list) or shell:
+        args = cmd 
+
+    process = subprocess.Popen(args, stdout=subprocess.PIPE,
+                               stderr=subprocess.STDOUT, shell=shell)
+    stdout = process.communicate()[0]
+
+    if 'error' in stdout:
+        sys.stdout.write(stdout)
+        
+    if procdir is not None:
+        os.chdir(cwd)
+
+    return stdout
+
 
 
 if __name__ == '__main__':
