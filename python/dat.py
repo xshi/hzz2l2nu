752924bda347370f4c326b30bf06e1d1f1bf118c
Xin Shi <xshi@xshi.org>
2014-11-13 15:47:08 -0500

    add

---
+++ python/dat.py
@@ -0,0 +1,27 @@
+"""
+General Data for analysis. 
+
+"""
+
+__author__ = "Xin Shi <Xin.Shi@cern.ch>"
+__copyright__ = "Copyright (c) Xin Shi"
+
+hltpaths = [
+    'HLT_Photon22_R9Id90_HE10_Iso40_EBOnly_PFMET40_v1',
+    'HLT_Photon22_R9Id90_HE10_Iso40_EBOnly_VBF_v1',
+    'HLT_Photon36_R9Id90_HE10_Iso40_EBOnly_PFMET40_v1',
+    'HLT_Photon36_R9Id90_HE10_Iso40_EBOnly_VBF_v1',
+    'HLT_Photon50_R9Id90_HE10_Iso40_EBOnly_PFMET40_v1',
+    'HLT_Photon50_R9Id90_HE10_Iso40_EBOnly_VBF_v1',
+    'HLT_Photon75_R9Id90_HE10_Iso40_EBOnly_PFMET40_v1',
+    'HLT_Photon75_R9Id90_HE10_Iso40_EBOnly_VBF_v1',
+    'HLT_Photon90_R9Id90_HE10_Iso40_EBOnly_PFMET40_v1',
+    'HLT_Photon90_R9Id90_HE10_Iso40_EBOnly_VBF_v1',
+    'HLT_Photon120_R9Id90_HE10_Iso40_EBOnly_PFMET40_v1',
+    'HLT_Photon120_R9Id90_HE10_Iso40_EBOnly_VBF_v1',
+    'HLT_Photon135_PFMET40_v1',
+    'HLT_Photon135_VBF_v1',
+    'HLT_Photon250_NoHE_v1',
+    'HLT_Photon300_NoHE_v1',
+]
+
